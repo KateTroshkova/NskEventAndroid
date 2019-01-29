80f8f6de0893e51a0696986be0af35cc39c3bc86
KateTroshkova <e-troshkova@inbox.ru>
2019-01-29 22:01:27 +0700

    merge conflict

--- app/src/main/java/winter2019/shift/nskevent_android/MainActivity.kt
+++ app/src/main/java/winter2019/shift/nskevent_android/MainActivity.kt
@@ -1,15 +1,15 @@
 package winter2019.shift.nskevent_android
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.*
+import android.widget.ListView
 
-import kotlinx.android.synthetic.main.activity_fragment.*
+import kotlinx.android.synthetic.main.activity_main.*
 import winter2019.shift.nskevent_android.model.Event
 import winter2019.shift.nskevent_android.presenter.ListFragmentPresenter
 import winter2019.shift.nskevent_android.presenter.MVPContract
+import winter2019.shift.nskevent_android.view.EventAdapter
 
 class MainActivity : AppCompatActivity(), MVPContract.ListView{
     override fun load(events: MutableList<Event>?) {
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity(), MVPContract.ListView{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content_main)
         setSupportActionBar(toolbar)
-        presenter= ListFragmentPresenter()
+        presenter=ListFragmentPresenter()
         presenter!!.attachView(this)
         presenter?.viewIsReady()
     }
@@ -63,4 +63,3 @@ class MainActivity : AppCompatActivity(), MVPContract.ListView{
         }
     }
 }
-
